aba05b2f084fbbf0ece5465c91cd3c62fdca0f6b
Mina Mikhael <mina.s.mikhael@gmail.com>
2022-08-06 17:22:36 +0200

    work in progress

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -1,8 +1,129 @@
 import React from "react";
 import "./portofolio.css";
+import img1 from "../../assets/image1.jpg";
+import img2 from "../../assets/image2.jpg";
+import img3 from "../../assets/image3.jpg";
+import img4 from "../../assets/image4.jpg";
+import img5 from "../../assets/image5.jpg";
+import img6 from "../../assets/image6.jpg";
 
 const Portofolio = () => {
-  return <section id="portfolio">Portofolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portofolio</h2>
+      <div className="container portofolio__container">
+        <article className="portofolio__item">
+          <div className="portofolio__item-image">
+            <img src={img1} alt="project 1" />
+          </div>
+          <h3>This is a portofolio item title</h3>
+          <div className="portofolio__item-cta">
+            <a href="https://github.com" className="btn">
+              Github
+            </a>
+            <a
+              href="https://dribble.com/alien_pixels"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary">
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portofolio__item">
+          <div className="portofolio__item-image">
+            <img src={img2} alt="project 1" />
+          </div>
+          <h3>This is a portofolio item title</h3>
+          <div className="portofolio__item-cta">
+            <a href="https://github.com" className="btn">
+              Github
+            </a>
+            <a
+              href="https://dribble.com/alien_pixels"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary">
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portofolio__item">
+          <div className="portofolio__item-image">
+            <img src={img3} alt="project 1" />
+          </div>
+          <h3>This is a portofolio item title</h3>
+          <div className="portofolio__item-cta">
+            <a href="https://github.com" className="btn">
+              Github
+            </a>
+            <a
+              href="https://dribble.com/alien_pixels"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary">
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portofolio__item">
+          <div className="portofolio__item-image">
+            <img src={img4} alt="project 1" />
+          </div>
+          <h3>This is a portofolio item title</h3>
+          <div className="portofolio__item-cta">
+            <a href="https://github.com" className="btn">
+              Github
+            </a>
+            <a
+              href="https://dribble.com/alien_pixels"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary">
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portofolio__item">
+          <div className="portofolio__item-image">
+            <img src={img5} alt="project 1" />
+          </div>
+          <h3>This is a portofolio item title</h3>
+          <div className="portofolio__item-cta">
+            <a href="https://github.com" className="btn">
+              Github
+            </a>
+            <a
+              href="https://dribble.com/alien_pixels"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary">
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portofolio__item">
+          <div className="portofolio__item-image">
+            <img src={img6} alt="project 1" />
+          </div>
+          <h3>This is a portofolio item title</h3>
+          <div className="portofolio__item-cta">
+            <a href="https://github.com" className="btn">
+              Github
+            </a>
+            <a
+              href="https://dribble.com/alien_pixels"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary">
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portofolio;
--- src/components/testemonial/Testemonial.jsx
+++ src/components/testemonial/Testemonial.jsx
@@ -1,8 +1,64 @@
 import React from "react";
 import "./testemonial.css";
+import avt1 from "../../assets/avatar1.jpg";
+import avt2 from "../../assets/avatar2.jpg";
+import avt3 from "../../assets/avatar3.jpg";
+import avt4 from "../../assets/avatar4.jpg";
 
 const Testemonial = () => {
-  return <section id="testemonial">Testemonial</section>;
+  return (
+    <section id="testemonial">
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={avt1} alt="avatar 1" />
+          </div>
+          <h5 className="client__name">Random Person 1</h5>
+          <small className="client__review">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, mollitia ex animi
+            eveniet nulla perferendis vero maxime. Beatae sint assumenda, omnis error adipisci
+            blanditiis, dolore eum culpa, fugiat qui maxime.
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={avt2} alt="avatar 2" />
+          </div>
+          <h5 className="client__name">Random Person 2</h5>
+          <small className="client__review">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, mollitia ex animi
+            eveniet nulla perferendis vero maxime. Beatae sint assumenda, omnis error adipisci
+            blanditiis, dolore eum culpa, fugiat qui maxime.
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={avt3} alt="avatar 3" />
+          </div>
+          <h5 className="client__name">Random Person 3</h5>
+          <small className="client__review">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, mollitia ex animi
+            eveniet nulla perferendis vero maxime. Beatae sint assumenda, omnis error adipisci
+            blanditiis, dolore eum culpa, fugiat qui maxime.
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={avt4} alt="avatar 4" />
+          </div>
+          <h5 className="client__name">Random Person 4</h5>
+          <small className="client__review">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, mollitia ex animi
+            eveniet nulla perferendis vero maxime. Beatae sint assumenda, omnis error adipisci
+            blanditiis, dolore eum culpa, fugiat qui maxime.
+          </small>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Testemonial;
