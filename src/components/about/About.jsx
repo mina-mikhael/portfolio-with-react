3634e60160ba3af106f1775e7b2183ee039c20d5
Mina Mikhael <mina.s.mikhael@gmail.com>
2022-08-08 00:43:23 +0200

    added some styles modifications

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import me from "../../assets/me.png";
+import me from "../../assets/me2.png";
 import { FaAward } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
 import { VscFolderLibrary } from "react-icons/vsc";
